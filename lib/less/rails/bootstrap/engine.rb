6de8e62a5103fdcd166d1bd5833f9564da9c194c
Thorben Schröder <stillepost@gmail.com>
2011-10-07 01:31:21 +0200

    Makes the engine compatible with Rails 3.1.1 initializer grouping

--- lib/less/rails/bootstrap/engine.rb
+++ lib/less/rails/bootstrap/engine.rb
@@ -2,9 +2,9 @@ module Less
   module Rails
     module Bootstrap
       class Engine < ::Rails::Engine
-        
-        config.after_initialize do |app|
-          bootstrap_less_files = config.root + 'vendor/stylesheets/twitter/bootstrap'
+
+        initializer 'less-rails-bootstrap.setup', :before => 'less-rails.after-setup', :group => :assets do |app|
+          bootstrap_less_files = config.root + 'vendor/assets/stylesheets/twitter'
           app.config.less.paths << bootstrap_less_files
         end
         
