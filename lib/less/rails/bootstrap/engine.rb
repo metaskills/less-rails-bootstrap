2d3d2a878cc78e3b66ede24b785c28989b0751d3
Dmitrij Mjakotnyi <kucaahbe@ukr.net>
2013-03-17 02:59:00 +0200

    fix for "undefined method `less'"

--- lib/less/rails/bootstrap/engine.rb
+++ lib/less/rails/bootstrap/engine.rb
@@ -1,14 +1,15 @@
 require 'rails'
+require 'less'
 
 module Less
   module Rails
     module Bootstrap
       class Engine < ::Rails::Engine
-        
+
         initializer 'less-rails-bootstrap.setup', :after => 'less-rails.after.load_config_initializers', :group => :all do |app|
           app.config.less.paths << File.join(config.root, 'vendor', 'frameworks')
         end
-        
+
       end
     end
   end
