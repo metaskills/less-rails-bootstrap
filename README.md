ded05b13dca2ea3ed46008278ee8c7067409756b
Josef Šimánek <retro@ballgag.cz>
2013-06-12 21:42:55 +0200

    Update bootstrap update script.

--- README.md
+++ README.md
@@ -81,10 +81,9 @@ We also have a test rails project setup using all the examples documented above.
 Here are the steps to update the version of twitter bootstrap used.
 
 * Clone this repository.
-* Clone the [bootstrap repo](https://github.com/twitter/bootstrap) in a twitter/bootstrap directory next to the clone of less-rails-bootstrap done above.
-* Change directories into the twitter/bootstrap clone and do a git checkout on the version you are targeting. For example `git checkout v2.3.0`. This will setup the proper version to copy over.
-* Back in the less-rails-bootstrap project. Run the `./scripts/update_bootstrap.sh` script.
+* Run the `./scripts/update_bootstrap.sh` script with bootstrap repo version tag argument. For example `./scripts/update_bootstrap.sh v2.3.2`.
 * RUN THE TESTS!
+* Send pull request.
 
 
 ## License
--- scripts/update_bootstrap.sh
+++ scripts/update_bootstrap.sh
@@ -1,29 +1,55 @@
 #!/bin/bash
 
-bs_dir='../twitter/bootstrap'
+bs_dir='./twitter/bootstrap'
 lrb_dir='.'
 lrb_img_dir="$lrb_dir/vendor/assets/images/twitter/bootstrap"
 lrb_js_dir="$lrb_dir/vendor/assets/javascripts/twitter/bootstrap"
 lrb_styles_dir="$lrb_dir/vendor/assets/stylesheets/twitter"
 lrb_fw_dir="$lrb_dir/vendor/frameworks/twitter/bootstrap"
 
-if [ ! -d $bs_dir ]; then
-    echo "Bootstrap not found. You might want to clone the bootstrap repo from http://twitter.github.com/bootstrap/ into $bs_dir"
-    exit 1
+error() {
+echo -e "\e[1;31m"$1"\e[0m"
+}
+
+success() {
+echo -e "\e[1;32m"$1"\e[0m"
+}
+
+info() {
+echo -e "\e[1;34m"$1"\e[0m"
+}
+
+if [ $# -ne 1 ] 
+then
+error "USAGE: $0 <bootstrap version>"
+exit 1
 fi
 
+if [ ! -d $bs_dir ]; then
+    info "Bootstrap not found. Cloning to twitter/bootstrap..."
+    git clone --quiet git://github.com/twitter/bootstrap.git twitter/bootstrap
+fi
+
+cd twitter/bootstrap
+info "Using $1..."
+git checkout --quiet $1
+cd ../../
+
+info "Copying icon fonts..."
 # images
 for f in $bs_dir/img/*; do
     bn=$(basename $f)
     cp $f $lrb_img_dir/$bn
 done
 
+info "Copying scripts..."
 # scripts
 for f in $bs_dir/js/*.js; do
     bn=$(basename $f)
     cp $f $lrb_js_dir/${bn/bootstrap-/}
 done
 
+info "Copying styles..."
 # styles
 for f in $bs_dir/less/*.less; do
     bn=$(basename $f)
@@ -31,3 +57,5 @@ for f in $bs_dir/less/*.less; do
         -e 's#background-image: url(#background-image: asset-url(#g' \
         $f > $lrb_fw_dir/$bn
 done
+
+success "Done"
