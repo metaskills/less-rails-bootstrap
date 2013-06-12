fa9e025c5e171f0de6adf7ec080f36bc11193db2
Josef Šimánek <retro@ballgag.cz>
2013-06-12 21:07:41 +0200

    Automatic bootstrap update script.

--- scripts/update_bootstrap.sh
+++ scripts/update_bootstrap.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
-bs_dir='../twitter/bootstrap'
+bs_dir='./twitter/bootstrap'
 lrb_dir='.'
 lrb_font_dir="$lrb_dir/vendor/assets/fonts/twitter/bootstrap"
 lrb_js_dir="$lrb_dir/vendor/assets/javascripts/twitter/bootstrap"
 lrb_styles_dir="$lrb_dir/vendor/assets/stylesheets/twitter"
 lrb_fw_dir="$lrb_dir/vendor/frameworks/twitter/bootstrap"
 
-if [ ! -d $bs_dir ]; then
-    echo "Bootstrap not found. You might want to clone the bootstrap repo from http://twitter.github.com/bootstrap/ into $bs_dir"
-    exit 1
+error() {
+echo -e "\e[1;31m"$1"\e[0m"
+}
+
+success() {
+echo -e "\e[1;32m"$1"\e[0m"
+}
+
+info() {
+echo -e "\e[1;34m"$1"\e[0m"
+}
+
+if [ $# -ne 1 ] 
+then
+error "USAGE: $0 <bootstrap version>"
+exit 1
 fi
 
+if [ ! -d $bs_dir ]; then
+    info "Bootstrap not found. Cloning to twitter/bootstrap..."
+    git clone --quiet git://github.com/twitter/bootstrap.git twitter/bootstrap
+fi
+
+cd twitter/bootstrap
+info "Using $1..."
+git checkout --quiet $1
+cd ../../
+
+info "Copying fonts..."
 # fonts
 mkdir -p $lrb_font_dir/
 for f in $bs_dir/fonts/*; do
@@ -19,12 +43,14 @@ for f in $bs_dir/fonts/*; do
     cp $f $lrb_font_dir/$bn
 done
 
+info "Copying scripts..."
 # scripts
 for f in $bs_dir/js/*.js; do
     bn=$(basename $f)
     cp $f $lrb_js_dir/${bn/bootstrap-/}
 done
 
+info "Copying styles..."
 # styles
 for f in $bs_dir/less/*.less; do
     bn=$(basename $f)
@@ -32,3 +58,5 @@ for f in $bs_dir/less/*.less; do
         -e 's#background-image: url(#background-image: asset-url(#g' \
         $f > $lrb_fw_dir/$bn
 done
+
+success "Done"
