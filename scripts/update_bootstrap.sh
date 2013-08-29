05a225cfdfab71f8f4dd8ce1eaa3089879137ca5
Maxim Dobryakov <mdobryakov@spbtv.com>
2013-08-29 19:12:41 +0400

    Fix update_bootstrap.sh for compatibility with v.3.0.0

--- scripts/update_bootstrap.sh
+++ scripts/update_bootstrap.sh
@@ -2,10 +2,17 @@
 
 bs_dir='./twitter/bootstrap'
 lrb_dir='.'
-lrb_font_dir="$lrb_dir/app/assets/fonts/twitter/bootstrap"
-lrb_js_dir="$lrb_dir/app/assets/javascripts/twitter/bootstrap"
-lrb_styles_dir="$lrb_dir/app/assets/stylesheets/twitter"
-lrb_fw_dir="$lrb_dir/app/frameworks/twitter/bootstrap"
+
+lrb_assets_dir="$lrb_dir/app/assets"
+lrb_font_dir="$lrb_assets_dir/fonts/twitter/bootstrap"
+lrb_js_dir="$lrb_assets_dir/javascripts/twitter/bootstrap"
+lrb_styles_dir="$lrb_assets_dir/stylesheets/twitter"
+
+lrb_bootstrap_js="$lrb_assets_dir/javascripts/twitter/bootstrap.js"
+lrb_bootstrap_less="$lrb_assets_dir/stylesheets/twitter/bootstrap.css.less"
+
+lrb_frameworks_dir="$lrb_dir/app/frameworks"
+lrb_fw_dir="$lrb_frameworks_dir/twitter/bootstrap"
 
 error() {
 echo -e "\e[1;31m"$1"\e[0m"
@@ -35,6 +42,12 @@ info "Using $1..."
 git checkout --quiet $1
 cd ../../
 
+info "Remove $lrb_assets_dir directory..."
+rm -rf $lrb_assets_dir
+
+info "Remove $lrb_frameworks_dir directory..."
+rm -rf $lrb_frameworks_dir
+
 info "Copying fonts..."
 # fonts
 mkdir -p $lrb_font_dir/
@@ -45,6 +58,7 @@ done
 
 info "Copying scripts..."
 # scripts
+mkdir -p $lrb_js_dir/
 for f in $bs_dir/js/*.js; do
     bn=$(basename $f)
     cp $f $lrb_js_dir/${bn/bootstrap-/}
@@ -52,11 +66,22 @@ done
 
 info "Copying styles..."
 # styles
+mkdir -p $lrb_fw_dir
 for f in $bs_dir/less/*.less; do
     bn=$(basename $f)
     sed -e "s/url('@{glyphicons-font-path}/asset-url('twitter\/bootstrap/g" \
         -e 's#background-image: url(#background-image: asset-url(#g' \
         $f > $lrb_fw_dir/$bn
 done
+
+info "Generate bootstrap.js..."
+for f in $lrb_js_dir/*.js; do
+    bn=$(basename $f)
+    echo "//= require twitter/bootstrap/$bn" >> $lrb_bootstrap_js
+done
+
+info "Generate bootstrap.css.less..."
+mkdir -p $lrb_styles_dir
+echo "@import \"twitter/bootstrap/bootstrap\";" >> $lrb_bootstrap_less
 
 success "Done"
