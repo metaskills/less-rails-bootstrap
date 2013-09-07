afba3bd21cfa334baa355644a1a7f2ebb5a5b63a
Maxim Dobryakov <mdobryakov@spbtv.com>
2013-09-07 15:42:28 +0400

    Check changes in auto-generated file by MD5 hash

--- test/lib/generators/custom_bootstrap_generator_test.rb
+++ test/lib/generators/custom_bootstrap_generator_test.rb
@@ -13,9 +13,12 @@ class CustomBootstrapGeneratorTest < Rails::Generators::TestCase
   end
 
   test "custom_bootstrap.less is generated" do
-    file_path = "app/assets/stylesheets/custom_bootstrap/custom_bootstrap.less"
+    file_path = destination_root.join("app/assets/stylesheets/custom_bootstrap/custom_bootstrap.less")
+
     assert_file(file_path, /@import/)
-    assert_equal 2312, File.size(File.expand_path(file_path, destination_root)), 'Looks like content of custom_bootstrap.less was changed. Please, investigate the reason of this and fix test if these changes is appropriate.'
+
+    md5 = Digest::MD5.hexdigest(File.read(file_path))
+    assert_equal '1f5b67b27608c7c5db14ad0b9c6cacbb', md5, 'Looks like content of custom_bootstrap.less was changed. Please, investigate the reason of this and fix test if these changes is appropriate.'
   end
 
   test "variables.less is generated" do
