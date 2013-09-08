a7c1383987291a946c92c7306137339937766058
Ken Collins <ken@metaskills.net>
2013-09-07 21:16:57 -0400

    Update spec helper along with dummy app helpers from less-rails.

--- test/dummy_app/init.rb
+++ test/dummy_app/init.rb
@@ -12,8 +12,10 @@ module Dummy
     config.cache_store = :memory_store
     config.consider_all_requests_local = true
     config.eager_load = false
-    config.assets.enabled = true
-    config.assets.cache_store = config.cache_store
+    
+    config.assets.enabled = true if ::Rails::VERSION::MAJOR < 4
+    config.assets.cache_store = [:file_store, "#{config.root}/tmp/cache/assets/"]
+    
   end
 end
 
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -5,35 +5,66 @@ require 'less-rails-bootstrap'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'dummy_app/init'
+require 'fileutils'
 
 module Less
   module Rails
     module Bootstrap
       class Spec < MiniTest::Spec
+        
+        include FileUtils
+        
+        class << self
+          
+          def dummy_app
+            Dummy::Application
+          end
+
+          def dummy_tmp
+            "#{dummy_app.root}/tmp"
+          end
+          
+        end
+        
         before do
+          prepare_cache_dir
+          reset_config_options
           reset_caches
         end
-
-        private
-
-        def dummy_app
-          Dummy::Application
-        end
+        
+        protected
+        
+        delegate :dummy_app, :dummy_tmp, :to => :'self.class'
 
         def dummy_config
           dummy_app.config
         end
-
+        
         def dummy_assets
           dummy_app.assets
         end
-
+        
         def dummy_asset(name)
           dummy_assets[name].to_s.strip
         end
-
+        
+        def reset_config_options
+          dummy_config.less.compress = false
+        end
+        
         def reset_caches
           dummy_assets.version = SecureRandom.hex(32)
+          cache = dummy_assets.cache
+          if cache.is_a? Sprockets::Cache::FileStore
+            path = cache.instance_variable_get(:@root)
+            cache = Sprockets::Cache::FileStore.new(path)
+          else
+            cache.clear
+          end
+        end
+        
+        def prepare_cache_dir
+          mkdir_p "#{dummy_tmp}/cache/assets"
         end
 
         def project_root
@@ -43,8 +74,9 @@ module Less
         def project_app_twitter_bootstrap
           File.join project_root, 'app', 'frameworks'
         end
+        
       end
-    end
+    end    
   end
 end
 
